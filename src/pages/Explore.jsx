222ecc652472baf90fef84eda7d2022e481e2308
inuoluwadunsimi <danielolaoladeinde@gmail.com>
2022-03-17 16:47:00 +0100

    added explore slider component

--- src/pages/Explore.jsx
+++ src/pages/Explore.jsx
@@ -1,4 +1,5 @@
 import {Link} from 'react-router-dom'
+import Slider from '../components/Slider'
 import rentCategoryImage from '../assets/jpg/rentCategoryImage.jpg'
 import sellCategoryImage from '../assets/jpg/sellCategoryImage.jpg'
 
@@ -11,7 +12,7 @@ function Explore() {
           <p className="pageHeader">Explore</p>
         </header>
         <main>
-          {/* {slider} */}
+          <Slider/>
 
           <p className="exploreCategoryHeading">Categories</p>
           <div className="exploreCategories">
