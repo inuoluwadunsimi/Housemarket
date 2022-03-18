601f1096adc7f88a735c14185afc8efa498614c1
inuoluwadunsimi <danielolaoladeinde@gmail.com>
2022-03-18 23:13:19 +0100

    added load more function

--- src/pages/Category.jsx
+++ src/pages/Category.jsx
@@ -10,6 +10,7 @@ function Category() {
 
     const[ listings,setListings] = useState(null)
     const [loading,setLoading] = useState(true)
+    const [lastFetchedListing,setLastFetchedListing] = useState(null)
 
     const params = useParams()
 
@@ -25,6 +26,10 @@ function Category() {
                 // query execution
 
                 const querySnap  = await getDocs(q)
+
+                const lastVisible = querySnap.docs[querySnap.docs.length - 1]
+
+                setLastFetchedListing(lastVisible)
 
                 const listings = []
 
@@ -45,6 +50,43 @@ function Category() {
         }
         fetchListings()
     },[params.categoryName])
+
+//Pagination Load more
+    const onFetchMoreListings = async () =>{
+        try {
+            //get ref
+            const listingsRef = collection(db,'listings')
+
+            //create query
+            const q = query(listingsRef,where('type','==',params.categoryName),orderBy('timestamp','desc'),startAfter(lastFetchedListing),limit(10))
+
+            // query execution
+
+            const querySnap  = await getDocs(q)
+
+            const lastVisible = querySnap.docs[querySnap.docs.length - 1]
+
+            setLastFetchedListing(lastVisible)
+
+            const listings = []
+
+            querySnap.forEach((doc)=>{
+               return listings.push({
+                   id:doc.id,
+                   data:doc.data(),
+               })
+            })
+
+            setListings((prevState)=>[...prevState,...listings])
+            setLoading(false)
+
+            
+        } catch (error) {
+            toast.error('Could not fetch listings')
+        }
+    }
+
+
   return (
     <div className='category'>
          <header>
@@ -63,6 +105,13 @@ function Category() {
                    ))}
                </ul>
            </main>
+
+           <br />
+           <br />
+
+           {lastFetchedListing && (
+               <p className="loadMore" onClick={onFetchMoreListings}>Load More</p>
+           )}
          </>
          )
          :( <p>No lisitngs for {params.categoryName}</p>)}
--- src/pages/Offers.jsx
+++ src/pages/Offers.jsx
@@ -10,6 +10,8 @@ function Offers() {
 
     const[ listings,setListings] = useState(null)
     const [loading,setLoading] = useState(true)
+    const [lastFetchedListing,setLastFetchedListing] = useState(null)
+
 
     const params = useParams()
 
@@ -25,6 +27,9 @@ function Offers() {
                 // query execution
 
                 const querySnap  = await getDocs(q)
+
+                const lastVisible = querySnap.docs[querySnap.docs.length - 1]
+
 
                 const listings = []
 
@@ -45,6 +50,43 @@ function Offers() {
         }
         fetchListings()
     },[])
+
+
+
+    const onFetchMoreListings = async () =>{
+        try {
+            //get ref
+            const listingsRef = collection(db,'listings')
+
+            //create query
+            const q = query(listingsRef,where('offer','==',true),orderBy('timestamp','desc'),startAfter(lastFetchedListing),limit(10))
+
+            // query execution
+
+            const querySnap  = await getDocs(q)
+
+            const lastVisible = querySnap.docs[querySnap.docs.length - 1]
+
+            setLastFetchedListing(lastVisible)
+
+            const listings = []
+
+            querySnap.forEach((doc)=>{
+               return listings.push({
+                   id:doc.id,
+                   data:doc.data(),
+               })
+            })
+
+            setListings((prevState)=>[...prevState,...listings])
+            setLoading(false)
+
+            
+        } catch (error) {
+            toast.error('Could not fetch listings')
+        }
+    }
+
   return (
     <div className='category'>
          <header>
@@ -63,6 +105,13 @@ function Offers() {
                    ))}
                </ul>
            </main>
+
+           <br />
+           <br />
+
+           {lastFetchedListing && (
+               <p className="loadMore" onClick={onFetchMoreListings}>Load More</p>
+           )}
          </>
          )
          :( <p>There are no current offers</p>)}
